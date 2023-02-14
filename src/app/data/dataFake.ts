adc7e955d10da1fe8d1fcae2d41501dc909faa85
Cristiano Agnaldo Mulinari <62948211+Crismulinari@users.noreply.github.com>
2023-02-14 14:54:12 -0300

    Abrindo outras paginas

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+  "id": "1",
+  "photoCover": "https://tse3.mm.bing.net/th?id=OIP.Ll1gTnmx3j8Ei0sT3QNS2AHaEo&pid=Api&P=0",
+  "title": "Minha noticia",
+  "description": "Olá Mundo"
+  },
+  {
+    "id": "2",
+    "photoCover": "aaa",
+    "title": "Saiu um novo Filme",
+    "description": "Olá Mundo"
+  },
+  {
+    "id": "3",
+    "photoCover": "aaa",
+    "title": "Saiu um novo Filme",
+    "description": "Olá Mundo"
+  },
+  {
+    "id": "4",
+    "photoCover": "aaa",
+    "title": "Saiu um novo Filme",
+    "description": "Olá Mundo"
+  }
+];
