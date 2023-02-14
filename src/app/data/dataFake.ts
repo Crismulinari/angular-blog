ac6574d9397a47659e7e84b0f335e66578c9caea
Cristiano Agnaldo Mulinari <62948211+Crismulinari@users.noreply.github.com>
2023-02-14 15:46:29 -0300

    content modified

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -7,20 +7,20 @@ export const dataFake = [
   },
   {
     "id": "2",
-    "photoCover": "aaa",
-    "title": "Saiu um novo Filme",
-    "description": "Olá Mundo"
+    "photoCover": "https://tse1.mm.bing.net/th?id=OIP.m2RouKk5qqNcf9ZqK2AiZwHaEM&pid=Api&P=0",
+    "title": "Star Wars TV Series Disney+ Streaming",
+    "description": "Várias series lançadas no streaming da Disney este ano."
   },
   {
     "id": "3",
-    "photoCover": "aaa",
+    "photoCover": "https://tse3.mm.bing.net/th?id=OIP.nn7tMlyD6LawAXp5cmEaigHaEK&pid=Api&P=0",
     "title": "Saiu um novo Filme",
-    "description": "Olá Mundo"
+    "description": "Até recentemente, o cronograma de lançamentos da Disney estava bastante instável, com várias datas a serem adiadas e outras sem sequer terem novas previsões. Do lado de Star Wars, a Disney emitiu um comunicado de imprensa, compartilhando que a série de Cassian Andor ainda está programada para ser lançada em 2021, e que a série de Obi-Wan Kenobi será lançado definitivamente em 2022."
   },
   {
     "id": "4",
-    "photoCover": "aaa",
-    "title": "Saiu um novo Filme",
+    "photoCover": "https://images.wallpapersden.com/image/download/star-wars-the-clone-wars-season-6_a2xqZ2qUmZqaraWkpJRmbmdlrWZlbWU.jpg",
+    "title": "Star Wars The Clone Wars Season 6 Wallpaper",
     "description": "Olá Mundo"
   }
 ];
