0dbf7c42d4864264b2c25f1d02ae8a2c133d8991
Cristiano Agnaldo Mulinari <62948211+Crismulinari@users.noreply.github.com>
2023-02-24 12:15:34 -0300

    Responsividade outras pagínas

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,25 +2,25 @@ export const dataFake = [
   {
   "id": "1",
   "photoCover": "https://tse3.mm.bing.net/th?id=OIP.Ll1gTnmx3j8Ei0sT3QNS2AHaEo&pid=Api&P=0",
-  "title": "Vader to appear in Star Wars spinoff",
-  "description": "While everyone’s favorite darksider probably won’t appear in the new trilogy (with him being dead and all), he will reportedly make an appearance in Star Wars Anthology: Rogue One, an intriguing prequel of sorts that follows a motley crew of rebels that steal the plans for the Death Star."
+  "title": "Darth Vader",
+  "description": "Darth Vader foi conhecido uma vez como Anakin Skywalker, um escravo do Planeta da Orla Externa de Tatooine. Quando criança, ele era talentoso em corridas de pod e conserto de androides. O Mestre Jedi Qui-Gon Jinn conheceu Anakin enquanto tentava consertar uma nave real de Naboo. E logo de cara, Qui-Gon percebeu o talento do menino para a mecânica e incrível habilidade para resolver problemas, bem como uma aptidão incrivelmente alta para a Força, e concluiu que ele era o escolhido mencionado em uma antiga profecia. Infelizmente, devido às manipulações de Darth Sidious, Anakin se tornaria um Jedi poderoso apenas para cair no Lado Negro. Em uma tentativa de destruir seu padawan caído, Obi-Wan Kenobi cortou os membros de Anakin e o deixou para morrer na beira de um rio de lava em Mustafar. Mas depois que Kenobi saiu, Sidious salvou Anakin, que renasceu como Darth Vader."
   },
   {
     "id": "2",
-    "photoCover": "https://tse1.mm.bing.net/th?id=OIP.m2RouKk5qqNcf9ZqK2AiZwHaEM&pid=Api&P=0",
-    "title": "Star Wars TV Series Disney+ Streaming",
-    "description": "Várias series lançadas no streaming da Disney este ano."
+    "photoCover": "https://tse1.mm.bing.net/th?id=OIP.Rt8kLLqowxtCbknsQseKygHaEe&pid=Api&P=0",
+    "title": "3ª temporada de The Mandalorian",
+    "description": "Após o término de O Livro de Boba Fett, a 3ª temporada de The Mandalorian será a responsável por desenvolver a história de Din Djarin e Grogu, nosso querido Baby Yoda. Nesta matéria, reunimos todas as informações sobre os novos episódios da série. A terceira temporada de The Mandalorian estreará no Disney+ em 1° de março de 2023. A primeira temporada estreou em novembro de 2019, enquanto a segunda em outubro de 2020."
   },
   {
     "id": "3",
     "photoCover": "https://tse3.mm.bing.net/th?id=OIP.nn7tMlyD6LawAXp5cmEaigHaEK&pid=Api&P=0",
-    "title": "Saiu um novo Filme",
-    "description": "Até recentemente, o cronograma de lançamentos da Disney estava bastante instável, com várias datas a serem adiadas e outras sem sequer terem novas previsões. Do lado de Star Wars, a Disney emitiu um comunicado de imprensa, compartilhando que a série de Cassian Andor ainda está programada para ser lançada em 2021, e que a série de Obi-Wan Kenobi será lançado definitivamente em 2022."
+    "title": "Obi-Wan Kenobi",
+    "description": "Apesar de ter sido muito criticada na época de seu lançamento por inúmeros motivos, os fãs também conseguiram enxergar os pontos positivos de Obi-Wan Kenobi, que traz à tona a história de um dos personagens mais icônicos da franquia— vivido por Alec Guinness e também por Ewan McGregor, que inclusive protagoniza essa série. Com isso, Obi-Wan vive uma aventura ao lado da pequena Princesa Leia (interpretada por Vivien Lyra Blair), que, ao ser sequestrada, precisa ser resgatada por uma pessoa de confiança e que entende tudo sobre a Força."
   },
   {
     "id": "4",
     "photoCover": "https://tse4.mm.bing.net/th?id=OIP.2XyIr04CBX0BhsYBZ3z3PwHaEK&pid=Api&P=0",
-    "title": "Star Wars The Clone Wars Season 6 Wallpaper",
-    "description": "Against a backdrop of ever-shifting loyalties, uneasy alliances and ancient hostilities, the conflict between the Republic and the Separatists ramps up, setting the stage for Darth Sidious' ultimate act of treachery against the Jedi. In these uncertain times, some of the deepest mysteries of the light and dark sides of the Force are revealed as an intrepid clone trooper uncovers a shocking conspiracy, Anakin Skywalker's closest relationship is tested to its limits, and Master Yoda makes a discovery that could forever change the balance of power in the galaxy."
+    "title": "Ambientada entre Episódio II: Ataque dos Clones e Episódio III: A Vingança dos Sith",
+    "description": "O universo Star Wars ganhou uma nova onda de popularidade no final dos anos 1990, com o lançamento da Trilogia Prelúdio formada pelos Episódios I, II e III. Além de marcar uma viagem ao passado da galáxia tão, tão distante, esses filmes chamaram a atenção para um período que foi expandido na TV com a série animada The Clone Wars. Lançada com um episódio especial em formato de filme, a animação é situada entre os Episódios II e III e conta como foram as famosas Guerras Clônicas com foco nos Jedi Obi-Wan Kenobi e Anakin Skywalker. A série é uma das responsáveis por ampliar a mitologia da saga, dando destaque a conceitos e personagens que ganharam pouco espaço nos filmes, além de apresentar novas figuras, como a queridinha Ahsoka Tano.."
   }
 ];
